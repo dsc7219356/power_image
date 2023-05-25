a4edbf1b074614cd77490512036e320cfd8d1a51
dsc <549252873@qq.com>
2023-05-25 10:36:46 +0800

    Initial commit

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,33 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<image_picker/FLTImagePickerPlugin.h>)
+#import <image_picker/FLTImagePickerPlugin.h>
+#else
+@import image_picker;
+#endif
+
+#if __has_include(<path_provider/FLTPathProviderPlugin.h>)
+#import <path_provider/FLTPathProviderPlugin.h>
+#else
+@import path_provider;
+#endif
+
+#if __has_include(<power_image/PowerImagePlugin.h>)
+#import <power_image/PowerImagePlugin.h>
+#else
+@import power_image;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+  [PowerImagePlugin registerWithRegistrar:[registry registrarForPlugin:@"PowerImagePlugin"]];
+}
+
+@end
---
+++ lib/src/common/power_image_binding.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:flutter/widgets.dart';
+import 'package:power_image/src/image_ext/image_cache_ext.dart';
+
+
+
+
+class PowerImageBinding extends WidgetsFlutterBinding {
+  @override
+  ImageCache createImageCache() {
+    // TODO: implement createImageCache
+    return ImageCacheExt();
+  }
+}
